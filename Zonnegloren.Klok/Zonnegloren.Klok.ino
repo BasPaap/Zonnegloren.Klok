463fddbf2f4ced4261719546bd0d4567e384276a
Bas Paap <baspaap@live.nl>
2023-08-15 21:33:07 +0200

    Create Arduino project.

---
+++ Zonnegloren.Klok/Zonnegloren.Klok.ino
@@ -0,0 +1,15 @@
+
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
